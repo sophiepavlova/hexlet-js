63bf8add30ee2ed0ef7d51cf4eb82b88dbfde4bb
sophiepavlova <olni.pavlova@gmail.com>
2024-05-16 13:13:15 -0700

    Modify eslint.config.js, index.js, package-lock.json, package.json

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import _ from "lodash";
-import summing from "./src/index.js";
+import _ from 'lodash';
+import summing from './src/index.js';
 
-console.log(_.last(["one", "two"]));
-console.log("Hello, Hexlet!");
+console.log(_.last(['one', 'two']));
+console.log('Hello, Hexlet!');
 
 const sumOfTwoElements = summing(10, 12);
 console.log(`the sum of two numbers is ${sumOfTwoElements}`);
